197a2d3e8313f465d27a0c892935ee4ee6615392
theobgbd <theogenius.pub@gmail.com>
2019-03-25 23:09:44 +0100

    added Line and Train classes prototypes

---
+++ line.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# Line class 
+class Line:
+    self.stations = list() # Stations IDs
+    self.trains   = list() # Trains IDs
+    self.color    = default_color
+    
+    def __init__(self, color):
+        self.color = color
+
+    def add_Station(new_station, target_station) :
+        self.stations_list = append(new_station)
+        
+    def remove_Station(old_station) :
+        self.station_list = remove(old_station)
+
+    def reorder_Stations() :
+        self.stations = shuffle()
+        
+    def add_Train() :
+    
